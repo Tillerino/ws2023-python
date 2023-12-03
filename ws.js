903aedc61da7347b6a96fb8aecd03ebcf6c93b6d
Tillmann Gaida <tillmann.gaida@gmail.com>
2023-12-03 11:28:00 +0100

    add slide feedback

---
+++ ws.js
@@ -0,0 +1,39 @@
+// In this file, we create the websocket that tracks user activity to assess the effectiveness of the slides, especially excercises.
+// We create a UUID and store that as a cookie so that reloading the page will not distort statistics.
+// We're not being excessively smart about this, but it's good enough unless somebody actively tries to distort the statistics.
+// We create a new UUID for every slide deck so that users cannot be tracked across slide decks.
+
+let deck = window.location.pathname.substr(window.location.pathname.lastIndexOf('/') + 1)
+var uuid = undefined
+var iws = undefined
+
+{
+	function getCookie(name) {
+		let value = '; ' + document.cookie
+		let parts = value.split('; ' + name + '=')
+		if (parts.length == 2) return parts.pop().split(';').shift()
+	}
+
+	function setCookie(name, value) {
+		document.cookie = name + '=' + value
+	}
+
+	uuid = getCookie('uuid-' + deck)
+	if (!uuid) {
+		setCookie('uuid-' + deck, crypto.randomUUID())
+		uuid = getCookie('uuid-' + deck)
+		// load again so that we know if we're loading from file://
+	}
+
+	if (uuid) {
+		iws = new ReconnectingWebSocket('wss://ws2023-python-interactive.pi.tillerino.org/interactive')
+	} else {
+		console.log('loaded locally')
+	}
+}
+
+function sendSlideChange(indexh, indexv) {
+	if (iws) {
+		iws.send(JSON.stringify({ '@type': 'Slide', uuid, deck, indexh, indexv }))
+	}
+}
